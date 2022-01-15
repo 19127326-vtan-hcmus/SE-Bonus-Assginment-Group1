b105e3aaa8910dfa347e81e0a5cb4e16d3555ed0
vuongthanhan-19127326 <64053104+vuongthanhan-19127326@users.noreply.github.com>
2022-01-15 11:16:57 +0700

    Iteration7

---
+++ moodledata/localcache/mustache/1641363180/boost/__Mustache_3c930c5845826ba610b9d166282d0e1d.php
@@ -0,0 +1,12 @@
+<?php
+
+class __Mustache_3c930c5845826ba610b9d166282d0e1d extends Mustache_Template
+{
+    public function renderInternal(Mustache_Context $context, $indent = '')
+    {
+        $buffer = '';
+
+
+        return $buffer;
+    }
+}
---
+++ moodledata/localcache/mustache/1641363180/boost/__Mustache_56292f2c4b80c537fde241d107d3a39e.php
@@ -0,0 +1,14 @@
+<?php
+
+class __Mustache_56292f2c4b80c537fde241d107d3a39e extends Mustache_Template
+{
+    public function renderInternal(Mustache_Context $context, $indent = '')
+    {
+        $buffer = '';
+
+        $buffer .= $indent . '
+';
+
+        return $buffer;
+    }
+}
