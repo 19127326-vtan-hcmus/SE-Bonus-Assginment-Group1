32f2f8ca4e5bcbc262373d7743cec1b7efba2b2e
vuongthanhan-19127326 <64053104+vuongthanhan-19127326@users.noreply.github.com>
2022-01-17 05:02:49 +0700

    Iteration11

---
+++ moodledata/localcache/mustache/1641363180/boost/__Mustache_d3fc5dbde5e1d18dbba3a2c0cbf3b930.php
@@ -0,0 +1,34 @@
+<?php
+
+class __Mustache_d3fc5dbde5e1d18dbba3a2c0cbf3b930 extends Mustache_Template
+{
+    public function renderInternal(Mustache_Context $context, $indent = '')
+    {
+        $buffer = '';
+
+        $buffer .= $indent . '<div class="container-fluid mb-2">
+';
+        $buffer .= $indent . '    <div class="row">
+';
+        $buffer .= $indent . '        <div class="col-xs-6 pr-1">
+';
+        $buffer .= $indent . '        </div>
+';
+        $buffer .= $indent . '        <div class="col-xs-6">
+';
+        $buffer .= $indent . '    
+';
+        $buffer .= $indent . '        </div>
+';
+        $buffer .= $indent . '    </div>
+';
+        $buffer .= $indent . '</div>
+';
+        $buffer .= $indent . '
+';
+        $buffer .= $indent . '
+';
+
+        return $buffer;
+    }
+}
